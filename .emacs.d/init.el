a3e7b2492c938be65246e014c297902a0b1b3116
codyne <30521850+Codyne@users.noreply.github.com>
2021-11-08 15:06:00 -0500

    update whitespace changes to init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,7 +14,13 @@
 (setq whitespace-line-column 80) ;; limit line length
 (setq whitespace-style '(face empty lines-tail trailing))
 (global-whitespace-mode t)
-(setq-default tab-width 8)
+(setq column-number-mode t)
+(setq-default indent-tabs-mode t)
+(setq-default tab-width 4)
+(setq-default c-basic-offset 4
+	      tab-width 4
+	      indent-tabs-mode t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;MISC SANE SETTINGS;;;;;;;;
